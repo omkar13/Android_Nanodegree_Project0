dcbf953bd8806ac50a8ada88c49351644dacdb83
omkar damle <omkarvdamle13@gmail.com>
2016-01-28 22:32:20 +0530

    first commit

---
+++ MyAppPortfolio/app/src/main/java/com/example/omkar/myappportfolio/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.omkar.myappportfolio;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button b1 = (Button) findViewById(R.id.button1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t1 = Toast.makeText(getApplication(), "This button will launch spotify streamer app ! ", Toast.LENGTH_SHORT);
+                t1.show();
+            }
+        });
+
+        Button b2 = (Button) findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t2 = Toast.makeText(getApplication(),"This button will launch scores app ! ",Toast.LENGTH_SHORT);
+                t2.show();
+            }
+        });
+
+
+        Button b3 = (Button) findViewById(R.id.button3);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t3 = Toast.makeText(getApplication(), "This button will launch library app ! ", Toast.LENGTH_SHORT);
+                t3.show();
+            }
+        });
+        Button b4 = (Button) findViewById(R.id.button4);
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t4 = Toast.makeText(getApplication(),"This button will launch build it bigger app ! ",Toast.LENGTH_SHORT);
+                t4.show();
+            }
+        });
+        Button b5 = (Button) findViewById(R.id.button5);
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t5 = Toast.makeText(getApplication(),"This button will launch xyz reader app ! ",Toast.LENGTH_SHORT);
+                t5.show();
+            }
+        });
+        Button b6 = (Button) findViewById(R.id.button6);
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t6 = Toast.makeText(getApplication(),"This button will launch Capstone: My own App app ! ",Toast.LENGTH_SHORT);
+                t6.show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
